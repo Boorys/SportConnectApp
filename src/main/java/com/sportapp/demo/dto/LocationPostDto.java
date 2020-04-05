6ed04d04f50525d6f8fbcfe70e49dd9557fbe80f
Kuba <kuba@email>
2020-04-05 18:42:58 +0200

    Add LocationPostDto

---
+++ src/main/java/com/sportapp/demo/dto/LocationPostDto.java
@@ -0,0 +1,99 @@
+package com.sportapp.demo.dto;
+
+
+public class LocationPostDto {
+
+    private String userId;
+    private String mainTypSportEntityId;
+    private String city;
+    private String country;
+    private String department;
+    private float lang;
+    private float lat;
+    private String street;
+    private String streetNumber;
+    private short hour;
+
+    public LocationPostDto() {
+    }
+
+    public short getHour() {
+        return hour;
+    }
+
+    public void setHour(short hour) {
+        this.hour = hour;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMainTypSportEntityId() {
+        return mainTypSportEntityId;
+    }
+
+    public void setMainTypSportEntityId(String mainTypSportEntityId) {
+        this.mainTypSportEntityId = mainTypSportEntityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public float getLang() {
+        return lang;
+    }
+
+    public void setLang(float lang) {
+        this.lang = lang;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+}
