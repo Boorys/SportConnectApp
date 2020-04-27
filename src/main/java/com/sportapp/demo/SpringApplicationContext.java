4d48a3dcf8b6aadf0d90de9260f330d9d665d980
Kuba <kuba@email>
2020-04-27 13:23:35 +0200

    Add GpsDto

--- src/main/java/com/sportapp/demo/SpringApplicationContext.java
+++ src/main/java/com/sportapp/demo/SpringApplicationContext.java
@@ -4,19 +4,25 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-public class SpringApplicationContext implements ApplicationContextAware
-{
-private static ApplicationContext CONTEXT;
+public class SpringApplicationContext implements ApplicationContextAware {
+    private static ApplicationContext CONTEXT;
 
-@Override
-public void setApplicationContext(ApplicationContext context) throws BeansException {
-	CONTEXT = context;
-	
-}
-	@Bean
-	public SpringApplicationContext springApplicationContext() {
-		return new SpringApplicationContext();
-	}
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        CONTEXT = context;
+
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public SpringApplicationContext springApplicationContext() {
+        return new SpringApplicationContext();
+    }
 
 }
---
+++ src/main/java/com/sportapp/demo/dto/GpsDto.java
@@ -0,0 +1,23 @@
+package com.sportapp.demo.dto;
+
+public class GpsDto {
+
+    private float lat;
+    private float lang;
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLang() {
+        return lang;
+    }
+
+    public void setLang(float lang) {
+        this.lang = lang;
+    }
+}
