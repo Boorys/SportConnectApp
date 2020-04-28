30f1cc31815725257c6a8a5dfa77407920ca1bc1
Kuba <kuba@email>
2020-04-28 14:09:30 +0200

    Add new method to return List<SelectedLocationPostDto>

--- src/main/java/com/sportapp/demo/controller/TakePartSportPlaceController.java
+++ src/main/java/com/sportapp/demo/controller/TakePartSportPlaceController.java
@@ -1,17 +1,17 @@
 package com.sportapp.demo.controller;
 
+import com.sportapp.demo.dto.SelectedLocationPostDto;
 import com.sportapp.demo.dto.LocationPostDto;
+import com.sportapp.demo.dto.SportFromLocalAreaDto;
 import com.sportapp.demo.service.LocationService;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.List;
 
 
 @RestController
@@ -20,12 +20,17 @@ public class TakePartSportPlaceController {
     @Autowired
     LocationService locationService;
 
-    @PostMapping(path="/add/location",consumes = {MediaType.APPLICATION_JSON_VALUE})
-    @ResponseStatus(value = HttpStatus.CREATED,reason = "Add location")
+    @PostMapping(path = "/add/location", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseStatus(value = HttpStatus.CREATED, reason = "Add location")
     public void addSportWishForUser(@RequestBody LocationPostDto locationPostDto) throws ParseException {
 
         locationService.saveLocation(locationPostDto);
     }
 
+    @GetMapping(path = "/get/all/sport/from/area/", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public List<SelectedLocationPostDto> getSportsLocationFromArea(@RequestBody SportFromLocalAreaDto sportFromLocalAreaDto) throws JSONException {
+
+        return locationService.getSportsLocationFromSelectedDistance(sportFromLocalAreaDto);
+    }
 
 }
